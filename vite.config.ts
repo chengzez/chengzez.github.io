c2d8412b94d9dabc06804a38aaa665af0d092140
chengzez <zcz20000909123@gmail.com>
2026-03-14 03:01:56 -0400

    Website

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        curriculum: resolve(__dirname, "curriculum.html"),
+        faculty: resolve(__dirname, "faculty.html"),
+      },
+    },
+  },
 });
